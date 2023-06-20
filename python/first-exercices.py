d909b18131814a2906e2a0ecc9425deaac7b9767
Lucas Apollo <apollohardrock@gmail.com>
2023-06-20 17:00:34 -0300

    exercícios 20-06

--- python/first-exercices.py
+++ python/first-exercices.py
@@ -5,3 +5,13 @@ print("Let's Go!")
 hoje = date.today()
 
 print(hoje)
+
+nomes = ['Maria', 'Julieta', 'Fernando', 'Cristiano', 'Julieta', 'Maria', 'Fernando', 'Cláudio']
+
+qtd_letras = {}
+
+for nome in nomes:
+    qtd_letras[nome] = len(nome)
+
+print(type(nomes))
+print(type(qtd_letras))
